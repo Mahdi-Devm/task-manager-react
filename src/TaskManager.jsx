c1cdd409a1c913427aa80fc09952dd6532e7a1ac
Mahdi <mahdibagherichanel@gmail.com>
2025-02-06 19:40:01 +0330

    Updated login form styles and fixed layout issues

--- src/TaskManager.jsx
+++ src/TaskManager.jsx
@@ -1,35 +1,5 @@
-import { FaGoogle } from "react-icons/fa";
+import Login from "./pages/Login";
 
 export default function TaskManager() {
-  return (
-    <div className="container">
-      <div className="main-container">
-        <form className="form">
-          <div className="Header-Form">
-            <h3>Create an Account</h3>
-            <p>
-              Already have an account? <a href="#">Log in</a>
-            </p>
-          </div>
-          <div>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Last Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-          </div>
-          <div className="input-Tik">
-            <input type="checkbox" />
-            <p>I agree to the terms and conditions</p>
-          </div>
-          <div className="Sugest-Form">
-            <button>Create Account</button>
-            <hr />
-            <button>
-              <FaGoogle /> Google
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+  return <Login />;
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,36 @@
-function Login() {
-  return <div></div>;
-}
+import { FaGoogle } from "react-icons/fa";
+import styles from "./Login.module.css";
 
-export default Login;
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainContainer}>
+        <form className={styles.form}>
+          <div className={styles.HeaderForm}>
+            <h3>Create an Account</h3>
+            <p>
+              Already have an account? <a href="#">Log in</a>
+            </p>
+          </div>
+          <div>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Last Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className={styles.inputTik}>
+            <input type="checkbox" />
+            <p>I agree to the terms and conditions</p>
+          </div>
+          <div className={styles.suggestForm}>
+            <button>Create Account</button>
+            <hr />
+            <button>
+              <FaGoogle /> Google
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
